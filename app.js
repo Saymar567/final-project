e5d8b50476498e01f226d827bbb64ce26b7bbcd8
Mikel Jimenez <majc1907@gmail.com>
2024-05-29 19:47:02 +0200

    first-day-final-commit

--- app.js
+++ app.js
@@ -25,7 +25,10 @@ const matchRoutes = require("./routes/match.routes");
 app.use("/matches", matchRoutes)
 
 const locationRoutes = require("./routes/location.routes")
-app.use("/location", locationRoutes)
+app.use("/locations", locationRoutes)
+
+const eventRoutes = require("./routes/event.routes")
+app.use("/events",eventRoutes)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- routes/location.routes.js
+++ routes/location.routes.js
@@ -7,7 +7,11 @@ router.post("/", (req, res)=>{
     .then((data)=>res.status(200).json({data}))
     .catch((error)=>res.status(400).json(error))
 })
-
+router.get("/", (req, res)=>{
+    Location.find()
+    .then((data)=>res.json(data))
+    .then((error)=>res.json(error))
+})
 
 
 
