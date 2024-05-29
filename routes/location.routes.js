7998b2afec006edc0361d1c1cd8bd6cef16db206
forsakenardy <153876743+forsakenardy@users.noreply.github.com>
2024-05-29 20:09:48 +0200

    Update location.routes.js

--- routes/location.routes.js
+++ routes/location.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Location = require("../models/Location.model");
 
-router.post("/locations", (req, res)=>{
+router.post("/location", (req, res)=>{
     Location.create(req.body)
     .then((data)=>res.status(200).json({data}))
     .catch((error)=>res.status(400).json(error))
