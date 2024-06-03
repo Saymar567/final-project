d4ac24431e9e7ddd64a5c7be4926ce5f22a118e9
Mikel Jimenez <majc1907@gmail.com>
2024-06-03 09:36:53 +0200

    locations-change-booking

--- models/Location.model.js
+++ models/Location.model.js
@@ -7,7 +7,8 @@ const locationSchema = new Schema({
     description: {type: String},
     rackets: {type: Boolean, default: false},
     net: {type: String},
-    barService: {type: Boolean, default: false} 
+    barService: {type: Boolean, default: false},
+    bookMatch: {type: mongoose.Schema.Types.ObjectId, ref: "User"} 
 })
 
 const Location = model("Location", locationSchema);
