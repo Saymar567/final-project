6a27784f7e9893bde6330982172d5e0b253a43d7
Nardy <nardy.allstars@gmail.com>
2024-06-04 09:24:51 +0200

    changes added

--- models/Location.model.js
+++ models/Location.model.js
@@ -1,15 +1,20 @@
-const mongoose = require("mongoose")
-const {Schema, model} = mongoose;
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 const locationSchema = new Schema({
-    place: {type: String},
-    image: {type: String},
-    description: {type: String},
-    rackets: {type: Boolean, default: false},
-    net: {type: String},
-    barService: {type: Boolean, default: false},
-    bookTable: {type: mongoose.Schema.Types.ObjectId, ref: "User"} 
-})
+    name: { type: String },
+    place: { type: String },
+    image: { type: String },
+    description: { type: String },
+    rackets: { type: Boolean, default: false },
+    net: { type: String },
+    barService: { type: Boolean, default: false },
+    horarios: [{
+        horaInicio: { type: String },
+        horaFin: { type: String },
+        reserved: { type: Boolean, default: false }
+    }]
+});
 
 const Location = model("Location", locationSchema);
 
